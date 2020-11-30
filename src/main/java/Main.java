6ff4d5a0eb562c00b6158b321852891c6f52306f
Dogan Sahin <dogansahin80@gmailcom>
2020-11-30 16:13:58 +0100

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+
+
+import ejb.EJBTestRemote;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String a[]) {
+        try {
+            Properties props = new Properties();
+
+            props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+            props.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+
+            Context context = new InitialContext(props);
+
+            EJBTestRemote etr = (EJBTestRemote) context.lookup("ejb:/TestEJB/EJBTest!ejb.EJBTestRemote");
+            System.out.println(etr.getName("Team A"));
+
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
