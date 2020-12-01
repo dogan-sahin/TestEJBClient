99bba8367373f4e63e4e075b448e7c0c3887068f
Dogan Sahin <dogansahin80@gmailcom>
2020-12-01 16:34:30 +0100

    added properties to connect to remote host

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,10 +14,17 @@ public class Main {
             Properties props = new Properties();
 
             props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-            props.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+            props.put(Context.PROVIDER_URL, "http-remoting://<host-address>:8080/");
+
+            /*
+            If the wildfly-server is running on a remote host, we have to create a
+            Wildfly Application-User on host -> $WILDFLY_HOME/add-user.sh
+            and provide the credentials of the created user as below
+            */
+            props.put(Context.SECURITY_PRINCIPAL, "username");
+            props.put(Context.SECURITY_CREDENTIALS, "password");
 
             Context context = new InitialContext(props);
-
             EJBTestRemote etr = (EJBTestRemote) context.lookup("ejb:/TestEJB/EJBTest!ejb.EJBTestRemote");
             System.out.println(etr.getName("Team A"));
 
